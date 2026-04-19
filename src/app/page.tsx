aa5c665b6a7bcc6e3bc0006795e1052576cdf87e
wesleyol <wesley.ol@gmail.com>
2026-04-19 00:34:12 -0300

    fix: simple page for build test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,8 @@
-<h1>PMToolkit.app - Em Construção</h1>.
+export default function Home() {
+  return (
+    <div>
+      <h1>PMToolkit Online</h1>
+      <p>Infraestrutura validada com sucesso.</p>
+    </div>
+  );
+}
