b3d0a5f98f73014ded8d0e7380d3c5fc472b45ab
wesleyol <wesley.ol@gmail.com>
2026-05-08 20:23:27 -0300

    fix: rename proxy.ts to middleware.ts via IDE to enforce i18n redirect

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,6 @@ import { routing } from './lib/i18n/routing';
 export default createMiddleware(routing);
 
 export const config = {
-  // Filtro robusto que ignora ficheiros de sistema (_next) e foca apenas nas rotas
   matcher: [
     '/',
     '/(pt|en|es)/:path*',
